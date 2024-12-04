894295ac9bac2b3043f71111daef1ec5e14f8fb6
karlrobeck <karlalferezfx@gmail.com>
2024-12-04 14:39:29 +0800

    fix modality reading-writing

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\FilesController;
 use App\Http\Controllers\ModalityVisualizationController;
 use App\Http\Controllers\User;
 use App\Http\Controllers\ModalityAuditoryController;
+use App\Http\Controllers\ModalityReadingWritingController;
 use App\Http\Controllers\ScoresController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,7 +12,7 @@ Route::apiResources([
     'users' => User::class,
     'files' => FilesController::class,
     'modality/visualizations' => ModalityVisualizationController::class,
-    'modality/reading-writing' => ModalityAuditoryController::class,
+    'modality/reading-writing' => ModalityReadingWritingController::class,
     'modality/auditory' => ModalityAuditoryController::class,
     'scores' => ScoresController::class,
 ]);
